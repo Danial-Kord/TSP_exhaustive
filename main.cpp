4fefaaa4be547a40412a7b37de0ee5b98da36703
Danial <danial.kordmodanlou@gmail.com>
2019-02-25 21:03:42 +0330

    execution time calculating added and showing the points that were selected

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include<math.h>
 #include <algorithm>
+#include <chrono>
+using namespace std::chrono;
 using namespace std;
 typedef struct point{
     double_t x;
@@ -11,7 +13,30 @@ typedef struct point{
         return output ;
     }
 }Point;
-vector<double> all_des;
+class Stopwatch
+{
+private:
+    steady_clock::time_point start;
+
+public:
+    Stopwatch(){
+        reset();
+    }
+
+    ~Stopwatch(){}
+
+    // Start the clock
+    void reset(){ start = steady_clock::now(); }
+
+    // Read time elapsed in nanoseconds
+    double getTime(){
+        //    return duration<double, std::nano>(steady_clock::now() - start).count();
+        return std::chrono::nanoseconds (steady_clock::now() - start).count();
+    }
+};
+
+double answer =-1;
+vector<Point> points;
 void permute(Point a[], int l, int r)
 {
 
@@ -19,25 +44,35 @@ void permute(Point a[], int l, int r)
         double des=0;
         for(int i =1;i<=r;i++){
             des += a[i-1].des(a[i]);
-            cout << " +=  : " << des;
+
         }
         des += a[r].des(a[0]);
-        cout << "des  " << des<<endl;
-        cout << endl;
-        all_des.push_back(des);
+        if(answer == -1) {
+            points.clear();
+            for (int i = 0; i <= r; ++i) {
+                points.push_back(a[i]);
+            }
+            points.push_back(a[0]);
+            answer = des;
+        }
+        else{
+            if(answer > des) {
+                answer = des;
+                points.clear();
+                for (int i = 0; i <= r; ++i) {
+                    points.push_back(a[i]);
+                }
+                points.push_back(a[0]);
+            }
+        }
     }
     else
     {
         for (int i = l; i <= r; i++)
-        {cout << a[l].x<<"  in "<<a[i].x<<endl;
-            cout << endl;
+        {
             swap(a[l], a[i]);
-            cout << a[l].x<<"  "<<a[i].x<<endl;
-            cout << endl;
             permute(a, l+1, r);
             swap(a[l], a[i]);
-            cout << a[l].x<<" 2 "<<a[i].x<<endl;
-            cout << endl;
         }
     }
 }
@@ -46,15 +81,20 @@ int main() {
     cin >> n;
     Point points[n];
     for (int i = 0; i < n; ++i) {
-        cin >> points[i].x;
-        cin >> points[i].y;
+//        cin >> points[i].x;
+//        cin >> points[i].y;
+        points[i].x = rand() % 20000;
+        points[i].y = rand() % 20000;
     }
-    permute(points,0,n-1);
-    sort(all_des.begin(),all_des.end());
-cout<< "..............."<<endl;
-    for (int j = 0; j < all_des.size(); ++j) {
-        cout << all_des.at(j) << endl;
+    Stopwatch sw;
+    permute(points,0,n-1);//O(n!)
+cout<< "........ result ......."<<endl;
+    cout << "points : [ " ;
+    for (int j = 0; j <n ; ++j) {//O(n^2)
+        cout << "x : " <<points->x << " y : "<<points->y<<",";
     }
-
+    cout << "]"<<endl;
+    cout << "min : "<< answer;
+    cout<<endl<< "#####Execution time: " <<sw.getTime() <<" nanoseconds" <<endl;
     return 0;
 }
